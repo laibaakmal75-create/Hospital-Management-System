a403e411a7e984f19ed6131c49a1311087d944e2
laiba akmal <laibaakmal75@gmail.com>
2026-05-13 13:25:07 +0500

    Update HospitalSystem.h

--- Project/HospitalSystem.h
+++ Project/HospitalSystem.h
@@ -1,6 +1,4 @@
 #pragma once
-
-#pragma once
 #include "Storage.h"
 #include "Patient.h"
 #include "Doctor.h"
@@ -8,68 +6,30 @@
 #include "Appointment.h"
 #include "Bill.h"
 #include "Prescription.h"
-#include "FileHandler.h"
-#include "Validator.h"
 
 class HospitalSystem {
-    Storage<Patient> patients;
-    Storage<Doctor> doctors;
-    Storage<Appointment> appointments;
-    Storage<Bill> bills;
-    Storage<Prescription> prescriptions;
-    Admin admin;
+private:
+
+    //each one holds up to 100 objects of the type 
+    //not for admin tho as admin is only 1 
+    Storage<Patient>      
+        patients;
+    Storage<Doctor>  
+        doctors;
+    Storage<Appointment> 
+        appointments;
+    Storage<Bill>    
+        bills;
+    Storage<Prescription> 
+        prescriptions;
+    Admin 
+        admin;
 
 public:
     HospitalSystem();
-
-    // Startup
     void loadAllData();
-    void run();
 
-    // Login
-    void showMainMenu();
-    Patient* loginPatient();
-    Doctor* loginDoctor();
-    bool loginAdmin();
-
-    // Patient actions
-    void patientMenu(Patient* p);
-    void bookAppointment(Patient* p);
-    void cancelAppointment(Patient* p);
-    void viewMyAppointments(Patient* p);
-    void viewMyMedicalRecords(Patient* p);
-    void viewMyBills(Patient* p);
-    void payBill(Patient* p);
-    void topUpBalance(Patient* p);
-
-    // Doctor actions
-    void doctorMenu(Doctor* d);
-    void viewTodaysAppointments(Doctor* d);
-    void markAppointmentComplete(Doctor* d);
-    void markAppointmentNoShow(Doctor* d);
-    void writePrescription(Doctor* d);
-    void viewPatientMedicalHistory(Doctor* d);
-
-    // Admin actions
-    void adminMenu();
-    void addDoctor();
-    void removeDoctor();
-    void addPatient();
-    void removePatient();
-    void viewAllPatients();
-    void viewAllDoctors();
-    void viewAllAppointments();
-    void viewUnpaidBills();
-    void dischargePatient();
-    void viewSecurityLog();
-    void generateDailyReport();
-
-    // Helpers
-    void getToday(char* dateStr);
-    void sortAppointmentsByDate(Appointment* arr, int n, bool ascending);
-    void sortPrescriptionsByDate(Prescription* arr, int n, bool ascending);
-    void caseInsensitiveCompare(const char* str1, const char* str2, bool& result);
-
+    //getters for sfml app to access storage 
     Storage<Patient>& getPatients() { return patients; }
     Storage<Doctor>& getDoctors() { return doctors; }
     Storage<Appointment>& getAppointments() { return appointments; }
